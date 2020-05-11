a490bab007ed52c6b52e959799562f920c3e3685
yanica svensson <sweets86_@hotmail.com>
2020-05-12 00:42:37 +0200

    some css

--- build/game.js
+++ build/game.js
@@ -1,6 +1,28 @@
 "use strict";
 function Intro() {
-    alert("Hey, Welcome");
+    alert("Vill du spela Dejta?");
 }
-Intro();
+// Intro();
+var MyObject = /** @class */ (function () {
+    function MyObject(genderMale, genderFemale, button) {
+        this.genderMale = genderMale;
+        this.genderFemale = genderFemale;
+        this.button = button;
+    }
+    return MyObject;
+}());
+var genderMale = document.getElementById("male").value;
+var genderFemale = document.getElementById("female").value;
+var button = document.getElementById("submitButton").value;
+// let confirm = new MyObject(gender, button);
+function myFunction(myObject) {
+    if (genderMale) {
+        document.getElementById("headerContainer").innerHTML = "";
+    }
+    else if (genderFemale) {
+        document.getElementById("headerContainer").innerHTML = "";
+    }
+}
+myFunction.onclick = function () {
+};
 //# sourceMappingURL=game.js.map
--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,35 @@
 function Intro() {
-    alert("Hey, Welcome");
+    alert("Vill du spela Dejta?");
 }
 
-Intro();
+// Intro();
+
+class MyObject {
+    genderMale: boolean;
+    genderFemale: boolean;
+    button: boolean;
+
+    constructor(genderMale: boolean, genderFemale: boolean, button: boolean) {
+        this.genderMale = genderMale;
+        this.genderFemale = genderFemale;
+        this.button = button;
+    }
+}
+
+let genderMale = (document.getElementById("male") as HTMLInputElement).value;
+let genderFemale = (document.getElementById("female") as HTMLInputElement).value;
+let button = (document.getElementById("submitButton") as HTMLInputElement).value;
+
+// let confirm = new MyObject(gender, button);
+
+function myFunction(myObject: MyObject) {
+    if (genderMale) {
+        (document.getElementById("headerContainer") as HTMLInputElement).innerHTML = ""
+    } else if (genderFemale) {
+        (document.getElementById("headerContainer") as HTMLInputElement).innerHTML = ""
+    }
+}
+
+myFunction.onclick = function () {
+    
+}
