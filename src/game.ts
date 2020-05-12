f76d3f5a56b133bc1bca1061d8f4c7142505cad1
yanica svensson <sweets86_@hotmail.com>
2020-05-12 19:26:11 +0200

    set #1 question

--- src/game.ts
+++ src/game.ts
@@ -6,7 +6,7 @@ function Intro() {
 
 
 
-function myFunction(props: any, text: any, mainCon: any) {
+function myFunction(props: any, text: any, mainConText: any, mainCon: any, list: any, img: any) {
 
     props = (document.getElementById("headerContainer") as HTMLInputElement).innerText ="";
     (document.body.append(props))
@@ -14,10 +14,53 @@ function myFunction(props: any, text: any, mainCon: any) {
     text = (document.getElementById("headerText") as HTMLInputElement).innerText ="";
     (document.body.append(text))
 
+    mainConText = (document.createElement("h1") as HTMLInputElement)
+    mainConText.innerText = "Börja Spela!";
+    mainConText.classList = "containerStartText";
+    (document.getElementById("mainContainer") as HTMLInputElement).append(mainConText)
+
     mainCon = (document.createElement("div") as HTMLInputElement)
-    mainCon.innerText = "Börja Spela!";
-    mainCon.classList = "containerText";
-    (document.getElementById("mainContainer") as HTMLInputElement).append(mainCon)
+    mainCon.innerText = "FRÅGA 1: Hur bjuder du ut din dejt?";
+    mainCon.classList = "containerQuery";
+    (document.getElementById("mainContainer") as HTMLInputElement).append(mainCon);
+
+    let text1 = {
+        answer: "1 - Promenad"
+    }
+    let text2 = {
+        answer: "2 - Bio"
+    }
+    let text3 = {
+        answer: "3 - Middag"
+    }
+
+    let questions: any = [text1, text2, text3]
+
+    for (let i = 0; i < questions.length; i++) {
+
+        let allQuestion: any = questions[i]
+
+        list = (document.createElement("li"))
+        list.innerText = allQuestion.answer;
+        list.onclick = function () {
+            if (allQuestion.answer[0] == 1) {
+                console.log("hej")
+            } else if (allQuestion.answer[0] == 2)  {
+                alert("hallo")
+            } else {
+                (document.getElementById("mainContainer") as HTMLInputElement).innerText = "";
+            }
+        };
+        /* list.classList = "listStyle"; */
+        (document.getElementById("mainContainer") as HTMLInputElement).append(list);
+        
+    }
+
+    img = (document.createElement("img"))
+    img.classList = "img"
+    img.src = "./build/Middag1.png";
+    (document.getElementById("mainContainer") as HTMLInputElement).append(img);
+
 }
 
 
