4a237ad33486f878cc24a2c172ee8ea158f1c34f
yanica svensson <sweets86_@hotmail.com>
2020-05-13 01:17:22 +0200

    finished up

--- src/game.js
+++ src/game.js
@@ -2,12 +2,23 @@ function Intro() {
     alert("Vill du spela Dejta?");
 }
 // Intro();
-function myFunction(props, text, mainConText, mainCon, list, img) {
+var text1 = {
+    answer: "1 - Promenad"
+};
+var text2 = {
+    answer: "2 - Bio"
+};
+var text3 = {
+    answer: "3 - Middag"
+};
+var questions = [text1, text2, text3];
+function myFunction(props, text, mainConText, mainCon, list, img, mainContainer) {
     props = document.getElementById("headerContainer").innerText = "";
     (document.body.append(props));
     text = document.getElementById("headerText").innerText = "";
     (document.body.append(text));
     mainConText = document.createElement("h1");
+    mainConText.id = "mainConID";
     mainConText.innerText = "Börja Spela!";
     mainConText.classList = "containerStartText";
     document.getElementById("mainContainer").append(mainConText);
@@ -16,33 +27,29 @@ function myFunction(props, text, mainConText, mainCon, list, img) {
     mainCon.innerText = "FRÅGA 1: Hur bjuder du ut din dejt?";
     mainCon.classList = "containerQuery";
     document.getElementById("mainContainer").append(mainCon);
-    var text1 = {
-        answer: "1 - Promenad"
-    };
-    var text2 = {
-        answer: "2 - Bio"
-    };
-    var text3 = {
-        answer: "3 - Middag"
-    };
-    var questions = [text1, text2, text3];
+    mainContainer = document.createElement("div");
+    mainContainer.id = "mainContainerID";
+    document.getElementById("mainContainer").append(mainContainer);
     var _loop_1 = function (i) {
         var allQuestion = questions[i];
         list = (document.createElement("li"));
+        list.id = "listID";
         list.innerText = allQuestion.answer;
         list.onclick = function () {
             if (allQuestion.answer[0] == 1) {
-                nextQuery(nextQuery);
+                document.getElementById("mainConID").innerText = "";
+                nextQuery(nextQuery, nextQuery, nextQuery);
             }
             else if (allQuestion.answer[0] == 2) {
-                alert("hallo");
+                document.getElementById("mainConID").innerText = "";
+                secondNextQ(secondNextQ, secondNextQ, secondNextQ);
             }
             else {
-                document.getElementById("mainContainer").innerText = "";
+                document.getElementById("mainConID").innerText = "";
             }
         };
         /* list.classList = "listStyle"; */
-        document.getElementById("mainContainer").append(list);
+        document.getElementById("mainContainerID").append(list);
     };
     for (var i = 0; i < questions.length; i++) {
         _loop_1(i);
@@ -53,9 +60,72 @@ function myFunction(props, text, mainConText, mainCon, list, img) {
     img.src = "./build/Middag1.png";
     document.getElementById("mainContainer").append(img);
 }
-function nextQuery(nextQ) {
+var textOne = {
+    answerOne: "1 - Tar fram Paraply",
+    ansOne: "1 - Titanic"
+};
+var textTwo = {
+    answerOne: "2 - Springer så fort du kan och tar skydd",
+    ansOne: "X - Saw XXX"
+};
+var textThree = {
+    answerOne: "3 - Vinkar på taxi",
+    ansOne: "2 - Partnern väljer"
+};
+var questionOne = [textOne, textTwo, textThree];
+function nextQuery(nextQ, nextL, nextImg) {
     nextQ = document.getElementById("mainID");
-    nextQ.innerText = "Det kommer nederbörd, vad gör du?";
+    nextQ.innerText = "FRÅGA 2: Det kommer nederbörd, vad gör du?";
+    var _loop_2 = function (i) {
+        var Question = questionOne[i];
+        nextL = document.getElementById("listID");
+        nextL.innerText = Question.answerOne;
+        nextL.onclick = function () {
+            if (Question.answerOne[0] == "1") {
+                alert("Ni lever lyckliga alla dar och gifter er ännu lyckligare. Spela igen?");
+            }
+            else if (Question.answerOne[0] == "2") {
+                alert("Oj, oj, oj.. Ni är dömda att aldrig mer ses igen, det var roligt så länge det vara. Spela igen?");
+            }
+            else {
+                alert("WoW! Ni skaffar barn ihop och lever loppan! Spela igen?");
+            }
+        };
+        document.getElementById("mainContainerID").append(nextL);
+    };
+    for (var i = 0; i < questionOne.length; ++i) {
+        _loop_2(i);
+    }
+    nextImg = document.getElementById("picID");
+    nextImg.classList = "img";
+    nextImg.src = "./build/Regn1.png";
+    document.getElementById("mainContainer").append(nextImg);
+}
+var tOne = {
+    ansOne: "1 - Titanic"
+};
+var tTwo = {
+    ansOne: "2 - Saw XXX"
+};
+var tThree = {
+    ansOne: "3 - Partnern väljer"
+};
+var questionTwo = [tOne, tTwo, tThree];
+function secondNextQ(secondQ, secondImg, secondL) {
+    secondQ = document.getElementById("mainID");
+    secondQ.innerText = "FRÅGA 2: Vilken film ser ni?";
+    for (var i = 0; i < questionTwo.length; ++i) {
+        var secQ = questionTwo[i];
+        secondL = document.getElementById("listID");
+        secondL.innerText = secQ.ansOne;
+        console.log(secQ.ansOne);
+    }
+    ;
+    document.getElementById("mainContainerID").append(secondL);
+    secondImg = document.getElementById("picID");
+    secondImg.classList = "img";
+    secondImg.src = "./build/Saw1.png";
+    document.getElementById("mainContainer").append(secondImg);
 }
 /* const button = (document.getElementById("submitButton") as HTMLInputElement);
 button.addEventListener("click", handleClick);
--- src/game.ts
+++ src/game.ts
@@ -1,19 +1,18 @@
 function Intro() {
     alert("Vill du spela Dejta?");
-}
+};
 
-// Intro();
-
+//Intro();
 
 
 let text1 = {
     answer: "1 - Promenad",
 }
 let text2 = {
-    answer: "2 - Bio",
+    answer: "X - Bio",
 }
 let text3 = {
-    answer: "3 - Middag",
+    answer: "2 - Middag",
 }
 
 let questions: any = [text1, text2, text3]
@@ -28,6 +27,7 @@ function myFunction(props: any, text: any, mainConText: any, mainCon: any, list:
     (document.body.append(text))
 
     mainConText = (document.createElement("h1") as HTMLInputElement)
+    mainConText.id = "mainConID"
     mainConText.innerText = "Börja Spela!";
     mainConText.classList = "containerStartText";
     (document.getElementById("mainContainer") as HTMLInputElement).append(mainConText)
@@ -48,15 +48,18 @@ function myFunction(props: any, text: any, mainConText: any, mainCon: any, list:
         let allQuestion: any = questions[i]
 
         list = (document.createElement("li"))
+        list.id = "listID"
         list.innerText = allQuestion.answer;
         list.onclick = function () {
-            if (allQuestion.answer[0] == 1) {
-                (document.getElementById("mainContainerID") as HTMLInputElement).innerText = "";
+            if (allQuestion.answer[0] == "1") {
+                (document.getElementById("mainConID") as HTMLInputElement).innerText = "Du spelar Dejta?";
                 nextQuery(nextQuery, nextQuery, nextQuery)
-            } else if (allQuestion.answer[0] == 2) {
-                alert("hallo")
+            } else if (allQuestion.answer[0] == "X") {
+                (document.getElementById("mainConID") as HTMLInputElement).innerText = "Du spelar Dejta?";
+                secondNextQ(secondNextQ, secondNextQ, secondNextQ)
             } else {
-                (document.getElementById("mainContainer") as HTMLInputElement).innerText = "";
+                (document.getElementById("mainConID") as HTMLInputElement).innerText = "Du spelar Dejta?";
+                dinnerQ(dinnerQ, dinnerQ, dinnerQ)
             }
         };
         /* list.classList = "listStyle"; */
@@ -73,13 +76,13 @@ function myFunction(props: any, text: any, mainConText: any, mainCon: any, list:
 }
 
 let textOne = {
-    answer1: "1 - Tar fram Paraply"
+    answerOne: "1 - Tar fram Paraply",
 }
 let textTwo = {
-    answer1: "2 - Springer så fort du kan och tar skydd"
+    answerOne: "X - Springer så fort du kan och tar skydd",
 }
 let textThree = {
-    answer1: "3 - Vinkar på taxi"
+    answerOne: "2 - Vinkar på taxi",
 }
 let questionOne = [textOne, textTwo, textThree]
 
@@ -90,8 +93,18 @@ function nextQuery(nextQ: any, nextL: any, nextImg: any) {
     for (let i = 0; i < questionOne.length; ++i) {
         let Question = questionOne[i]
 
-        nextL = document.createElement("li")
-        nextL.innerText = Question.answer1;
+        nextL = document.getElementById("listID")
+        nextL.innerText = Question.answerOne;
+        nextL.onclick = function () {
+            if (Question.answerOne[0] == "1") {
+                alert("Ni lever lyckliga alla dar och gifter er ännu lyckligare. Spela igen?")
+            } else if (Question.answerOne[0] == "X") {
+                alert("Oj, oj, oj.. Ni är dömda att aldrig mer ses igen, det var roligt så länge det vara. Spela igen?")
+            } else {
+                alert("WoW! Ni skaffar barn ihop och lever loppan! Spela igen?")
+            }
+            location.reload()
+        };
         (document.getElementById("mainContainerID") as HTMLInputElement).append(nextL);
     }
 
@@ -104,7 +117,144 @@ function nextQuery(nextQ: any, nextL: any, nextImg: any) {
 }
 
 
+let tOne = {
+    ansOne: "1 - Titanic"
+}
+let tTwo = {
+    ansOne: "X - Saw XXX"
+}
+let tThree = {
+    ansOne: "2 - Partnern väljer"
+}
 
+let questionTwo = [tOne, tTwo, tThree]
+
+function secondNextQ(secondQ: any, secondImg: any, secondL: any) {
+    secondQ = document.getElementById("mainID")
+    secondQ.innerText = "FRÅGA 2: Vilken film ser ni?"
+
+    for (let i = 0; i < questionTwo.length; ++i) {
+        let secQ = questionTwo[i]
+
+        secondL = document.getElementById("listID")
+        secondL.innerText = secQ.ansOne;
+        secondL.onclick = function () {
+            movieQ(movieQ, movieQ, movieQ)
+        };
+
+        (document.getElementById("mainContainerID") as HTMLInputElement).append(secondL);
+    }
+
+
+    secondImg = document.getElementById("picID")
+    secondImg.classList = "img"
+    secondImg.src = "./build/Saw1.png";
+
+    (document.getElementById("mainContainer") as HTMLInputElement).append(secondImg);
+}
+
+let mOne = {
+    aOne: "1 - Närmare kontakt",
+    bOne: "1 - Hemma",
+    cOne: "1 - 2 st."
+}
+let mTwo = {
+    aOne: "X - Det behövs mer popcorn",
+    bOne: "X - Paris",
+    cOne: "X - 5 st."
+}
+let mThree = {
+    aOne: "2 - Brandlarmet går",
+    bOne: "2 - Vid sjön",
+    cOne: "2 - 1 st."
+}
+
+let questionThree = [mOne, mTwo, mThree]
+
+function movieQ(thirdQ: any, thirdImg: any, thirdL: any) {
+    thirdQ = document.getElementById("mainID")
+    thirdQ.innerText = "FRÅGA 3: Vad händer under filmen?"
+
+    for (let i = 0; i < questionThree.length; i++) {
+        let threeQ = questionThree[i]
+
+        thirdL = document.getElementById("listID")
+        thirdL.innerText = threeQ.aOne;
+        thirdL.onclick = function () {
+            if (threeQ.aOne[0] == "1") {
+                alert("YEAH! Ni skaffar barn ihop och lever loppan! Spela igen?")
+            } else if (threeQ.aOne[0] == "X") {
+                alert("Bra val, ni lever lyckliga alla dar och gifter er ännu lyckligare. Spela igen?")
+            } else {
+                alert("Ojdå! Ni är dömda att aldrig mer ses igen, det var roligt så länge det vara. Spela igen?")
+            }
+            location.reload()
+        };
+
+        (document.getElementById("mainContainerID") as HTMLInputElement).append(thirdL);
+    }
+
+    thirdImg = document.getElementById("picID")
+    thirdImg.classList = "img"
+    thirdImg.src = "./build/Popcorn1.png";
+
+    (document.getElementById("mainContainer") as HTMLInputElement).append(thirdImg);
+}
+
+
+
+function dinnerQ(fourtQ: any, fourthImg: any, fourthL: any) {
+    fourtQ = document.getElementById("mainID")
+    fourtQ.innerText = "FRÅGA 2: Vart tar du din dejt på middag?"
+
+    for (let i = 0; i < questionThree.length; i++) {
+        let fourQ = questionThree[i]
+
+        fourthL = document.getElementById("listID")
+        fourthL.innerText = fourQ.bOne;
+        fourthL.onclick = function () {
+            kidzQ(kidzQ, kidzQ, kidzQ)
+        };
+
+        (document.getElementById("mainContainerID") as HTMLInputElement).append(fourthL);
+    }
+
+    fourthImg = document.getElementById("picID")
+    fourthImg.classList = "img"
+    fourthImg.src = "./build/Paris1.png";
+
+    (document.getElementById("mainContainer") as HTMLInputElement).append(fourthImg);
+}
+
+function kidzQ(fifthQ: any, fifthImg: any, fifthL: any) {
+    fifthQ = document.getElementById("mainID")
+    fifthQ.innerText = "FRÅGA 3: Hur många barn vill du ha?"
+
+    for (let i = 0; i < questionThree.length; i++) {
+        let fiveQ = questionThree[i]
+
+        fifthL = document.getElementById("listID")
+        fifthL.innerText = fiveQ.cOne;
+        fifthL.onclick = function () {
+            if (fiveQ.cOne[0] == "1") {
+                alert("Javisst, ni lever lyckliga alla dar och gifter er ännu lyckligare. Spela igen?")
+            } else if (fiveQ.cOne[0] == "X") {
+                alert("Ooops... Ni är dömda att aldrig mer ses igen, det var roligt så länge det vara. Spela igen?")
+            } else {
+                alert("Det här gick ju bra! Ni skaffar barn ihop och lever loppan! Spela igen?")
+            }
+            location.reload()
+        };
+
+        (document.getElementById("mainContainerID") as HTMLInputElement).append(fifthL);
+    }
+
+    fifthImg = document.getElementById("picID")
+    fifthImg.classList = "img"
+    fifthImg.src = "./build/Baby1.png";
+
+    (document.getElementById("mainContainer") as HTMLInputElement).append(fifthImg);
+}
 
 
 
