5c9c954372d919df2aa1635b90c662a12586d7bc
yanica svensson <sweets86_@hotmail.com>
2020-05-12 17:14:16 +0200

    display text in frontend with onclick function

--- build/game.js
+++ build/game.js
@@ -3,26 +3,49 @@ function Intro() {
     alert("Vill du spela Dejta?");
 }
 // Intro();
-var MyObject = /** @class */ (function () {
-    function MyObject(genderMale, genderFemale, button) {
+function myFunction(props, text, mainCon) {
+    props = document.getElementById("headerContainer").innerText = "";
+    (document.body.append(props));
+    text = document.getElementById("headerText").innerText = "";
+    (document.body.append(text));
+    mainCon = document.createElement("div");
+    mainCon.innerText = "Börja Spela!";
+    mainCon.classList = "containerText";
+    document.getElementById("mainContainer").append(mainCon);
+}
+/* const button = (document.getElementById("submitButton") as HTMLInputElement);
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+    console.log("Clicked!")
+} */
+/* let button = (document.getElementById("submitButton") as HTMLInputElement);
+button.addEventListener("click", () => {
+    (document.getElementById("headerContainer") as HTMLInputElement).innerText =""
+}); */
+/* class MyObject {
+    genderMale: boolean;
+    genderFemale: boolean;
+    button: boolean;
+
+    constructor(genderMale: boolean, genderFemale: boolean, button: boolean) {
         this.genderMale = genderMale;
         this.genderFemale = genderFemale;
         this.button = button;
     }
-    return MyObject;
-}());
-var genderMale = document.getElementById("male").value;
-var genderFemale = document.getElementById("female").value;
-var button = document.getElementById("submitButton").value;
-// let confirm = new MyObject(gender, button);
-function myFunction(myObject) {
-    if (genderMale) {
-        document.getElementById("headerContainer").innerHTML = "";
-    }
-    else if (genderFemale) {
-        document.getElementById("headerContainer").innerHTML = "";
-    }
 }
+
+let genderMale = (document.getElementById("male") as HTMLInputElement).value;
+let genderFemale = (document.getElementById("female") as HTMLInputElement).value;
+let button = (document.getElementById("submitButton") as HTMLInputElement).value;
+
+// let confirm = new MyObject(gender, button);
+
+function myFunction(myObject: MyObject) {
+
+}
+
 myFunction.onclick = function () {
-};
+
+} */
 //# sourceMappingURL=game.js.map
--- src/game.js
+++ src/game.js
@@ -1,80 +1,23 @@
 function Intro() {
     alert("Vill du spela Dejta?");
 }
-
 // Intro();
-
-
-
-function myFunction(props: any, text: any, mainCon: any) {
-
-    props = (document.getElementById("headerContainer") as HTMLInputElement).innerText ="";
-    (document.body.append(props))
-
-    text = (document.getElementById("headerText") as HTMLInputElement).innerText ="";
-    (document.body.append(text))
-
-    mainCon = (document.createElement("div") as HTMLInputElement)
-    mainCon.innerText = "Börja Spela!";
-    mainCon.classList = "containerText";
-    (document.getElementById("mainContainer") as HTMLInputElement).append(mainCon)
+function myFunction(props) {
+    if (props.push) {
+        props = document.getElementById("headerContainer").innerText = "";
+    }
+    console.log("hey");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* const button = (document.getElementById("submitButton") as HTMLInputElement);
 button.addEventListener("click", handleClick);
 
 function handleClick() {
     console.log("Clicked!")
 } */
-
-
 /* let button = (document.getElementById("submitButton") as HTMLInputElement);
 button.addEventListener("click", () => {
     (document.getElementById("headerContainer") as HTMLInputElement).innerText =""
 }); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* class MyObject {
     genderMale: boolean;
     genderFemale: boolean;
@@ -100,4 +43,3 @@ function myFunction(myObject: MyObject) {
 myFunction.onclick = function () {
 
 } */
-
